704756117ab1bca13009d41ed7abcdfcbb5944c0
Piyush7065392044 <129537005+Piyush7065392044@users.noreply.github.com>
2025-03-12 00:26:38 +0530

    function simple and advance

---
+++ function.dart
@@ -0,0 +1,34 @@
+// greeting function ;
+// void greeting() {
+//   // creating a function
+
+//   print("hello , world "); // writing a logic
+// }
+
+// void main() {
+//   // calling  a function
+//   greeting(); // by there name
+// }
+
+//  input  function in dart
+// import 'dart:io' show stdin;
+
+// void main() {
+//   print("Enter your name:");
+//   String? name = stdin.readLineSync();  // User से input लेना
+//   print("Hello, $name!");
+// }
+
+// sum function
+// use void for not returnning a
+
+void sum() {
+  int a = 10;
+  int b = 20;
+  int sum = a * b;
+  print(sum);
+}
+
+void main() {
+  sum();
+}
